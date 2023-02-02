2bddd51ef475bb42d3f49429927fb75c629e87d2
CISAC <carolisacosta99@gmail.com>
2023-02-02 15:56:17 +0000

    Fixed Power-Up Window

--- Assets/_Game/Editors/PowerUpEditorWindow.cs
+++ Assets/_Game/Editors/PowerUpEditorWindow.cs
@@ -13,24 +13,24 @@ namespace Editors
         private VisualTreeAsset m_VisualTreeAsset = default;
 
         [MenuItem("Window/UI Toolkit/PowerUpEditor")]
-        public static void CreateMenu()
+        public static void ShowExample()
         {
             var wnd = GetWindow<PowerUpEditorWindow>();
             wnd.titleContent = new GUIContent("PowerUpEditor");
         }
         
-        /*public void CreateGUI()
+        public void CreateGUI()
         {
             // Each editor window contains a root VisualElement object
             VisualElement root = rootVisualElement;
 
             // VisualElements objects can contain other VisualElement following a tree hierarchy.
-            VisualElement label = new Label("Hello World! From C#");
-            root.Add(label);
+            //VisualElement label = new Label("Hello World! From C#");
+            //root.Add(label);
 
             // Instantiate UXML
             VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
             root.Add(labelFromUXML);
-        }*/
+        }
     }
 }
